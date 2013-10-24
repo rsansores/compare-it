65dabdc2d79380a609014e6b117f841c9ef8cd69
Ignacio Quintero <nacho950@gmail.com>
2013-10-24 05:39:20 +0000

    Barcode + icono Signed-off-by: Ignacio Quintero <nacho950@gmail.com>

--- CompareIT/CompareIT/src/main/java/com/example/compareit/addProduct.java
+++ CompareIT/CompareIT/src/main/java/com/example/compareit/addProduct.java
@@ -1,10 +1,13 @@
 package com.example.compareit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by Nacho on 10/23/13.
@@ -19,7 +22,29 @@ public class addProduct extends Fragment {
                 R.layout.addproduct, container, false);
 
 
+        final ImageButton button = (ImageButton) vi.findViewById(R.id.pick_barcode);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               barCode();
+            }
+        });
+
+
+
         return vi;
     }
+
+
+    public void barCode() {
+        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+        intent.putExtra("SCAN_MODE", "BAR_CODE_MODE");
+        startActivityForResult(intent, 0);
+    }
+
+
+
+
+
+
 
 }
