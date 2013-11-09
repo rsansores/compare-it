7758b7055faf80746e02197ee6b3806064f0cb2e
Obed N Munoz <obed.n.munoz@gmail.com>
2013-11-08 23:00:02 -0500

    base de datos

--- CompareIT-api/rest_api/models.py
+++ CompareIT-api/rest_api/models.py
@@ -1,10 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Product(models.Model):
-    
-  name = models.CharField(max_length=30)
-  
-  def __unicode__(self):
-      return self.name
-       
+
+class Reliability(models.Model):
+	name = models.CharField(max_length=45)
+
+class Enterprise(models.Model):
+	name = models.CharField(max_length=45)
+	created = models.DateField()
+	modified = models.DateTimeField(auto_now_add=True)
+	deleted = models.IntegerField()
+	description = models.CharField(max_length=50)
+	latitude = models.CharField(max_length=100)
+	longitude = models.CharField(max_length=100)
+
+class Objects(models.Model):
+	name = models.CharField(max_length=45)
+	price = models.DecimalField(max_digits=9, decimal_places=2)
+	isPromotion = models.IntegerField()
+	discount = models.DecimalField(max_digits=9, decimal_places=2)
+	created = models.DateField()
+	modified = models.DateTimeField(auto_now_add=True)
+	deleted = models.IntegerField()
+	description = models.CharField(max_length=50)
+	ticketImage = models.CharField(max_length=100)
+	objectImage = models.CharField(max_length=100)
+	enterpriseId = models.ForeignKey(Enterprise)
+	userId = models.ForeignKey(User)
+	reliabilityId = models.ForeignKey(Reliability)
