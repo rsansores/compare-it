8624da43b567a70eea137ae38d4981cb47ff13f8
Obed N Munoz <obed.n.munoz@gmail.com>
2013-11-11 23:55:27 -0500

    Adding ImageField for Product and Ticket Images

--- CompareIT-api/CompareIT/urls.py
+++ CompareIT-api/CompareIT/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from rest_framework import routers
 from rest_api import views
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -24,4 +25,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rest-api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
--- CompareIT-api/rest_api/models.py
+++ CompareIT-api/rest_api/models.py
@@ -4,27 +4,37 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Reliability(models.Model):
-	name = models.CharField(max_length=45)
+    name = models.CharField(max_length=45)
+
+    def __unicode__(self):
+        return self.name 
 
 class Enterprise(models.Model):
-	name = models.CharField(max_length=45)
-	created = models.DateField()
-	modified = models.DateTimeField(auto_now_add=True)
-	deleted = models.IntegerField()
-	description = models.CharField(max_length=50)
-	latitude = models.CharField(max_length=100)
-	longitude = models.CharField(max_length=100)
+    name = models.CharField(max_length=45)
+    created = models.DateField()
+    modified = models.DateTimeField(auto_now_add=True)
+    deleted = models.IntegerField()
+    description = models.CharField(max_length=50)
+    latitude = models.CharField(max_length=100)
+    longitude = models.CharField(max_length=100)
+   
+    def __unicode__(self):
+        return self.name 
 
 class Product(models.Model):
-	name = models.CharField(max_length=45)
-	price = models.DecimalField(max_digits=9, decimal_places=2)
-	isPromotion = models.IntegerField()
-	discount = models.DecimalField(max_digits=9, decimal_places=2)
-	created = models.DateField()
-	modified = models.DateTimeField(auto_now_add=True)
-	deleted = models.IntegerField()
-	description = models.CharField(max_length=50)
-	ticketImage = models.CharField(max_length=100)
-	objectImage = models.CharField(max_length=100)
-	enterpriseId = models.ForeignKey(Enterprise)
-	reliabilityId = models.ForeignKey(Reliability)
+     
+    name = models.CharField(max_length=45)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    is_promotion = models.IntegerField()
+    discount = models.DecimalField(max_digits=9, decimal_places=2)
+    created = models.DateField()
+    modified = models.DateTimeField(auto_now_add=True)
+    deleted = models.IntegerField()
+    description = models.CharField(max_length=50)
+    ticket_image = models.ImageField(upload_to = 'images/tickets/')  
+    product_image = models.ImageField(upload_to = 'images/products/')
+    enterprise = models.ForeignKey(Enterprise)
+    reliability = models.ForeignKey(Reliability)
+
+    def __unicode__(self):
+        return self.name 
